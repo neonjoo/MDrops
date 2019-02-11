a3a1e0eab064c7cc4745cfb108468a4ac8d25978
anpasti <aps1994@gmail.com>
2019-02-11 18:08:43 +0200

    created edge finder, and added adaptive time step

---
+++ mesh_functions.jl
@@ -0,0 +1,33 @@
+function make_edges(faces)
+    edges = Array{Int64}(undef, 2, 0)
+    for i in 1:size(faces,2) # faces
+        face = faces[:,i]
+        for j in 1:size(face,1) # vertices in face
+            edge = sort([face[j],face[j%3+1]])
+
+            ##check if edge is in edges
+            duplicate = false
+            for k = 1:size(edges,2)
+                if edge == edges[:,k]
+                    duplicate = true
+                    break
+                end
+            end
+
+            if !duplicate
+                edges = cat(edges,edge,dims=2)
+            end
+
+        end
+    end
+    return edges
+end
+
+function make_edge_lens(points,edges)
+    edge_lens = Array{Float64}(undef, size(edges,2))
+    for i = 1:size(edges,2)
+        edge_vec = points[:,edges[1,i]] - points[:,edges[2,i]]
+        edge_lens[i] = norm(edge_vec)
+    end
+    return edge_lens
+end
